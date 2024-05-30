fc5ec66a5612d35dbcf078adea1c3339b18c4ec1
ibartlett2 <ibartlett2@sycamores.indstate.edu>
2024-05-30 03:14:10 -0400

    Updated docstrings and typing.
    I'm not sure if the typing is correct, but it functions so far.

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import os
 import sys
 import asyncio
 
-from discord import Interaction, Intents, app_commands
+from discord import Interaction, Intents, errors
 from discord.ext import commands
+from discord.ext.commands import Context
 
 from global_utils import global_utils
 
@@ -12,23 +13,29 @@ bot = commands.Bot(command_prefix='!',
 
 @bot.event
 async def on_ready():
+    """[event] Logs the bot's connection to Discord and starts the log file
+    """
     sys.stderr = open(f'./logs/{global_utils.last_log_date}_stderr.log', 'a')
 
     global_utils.log(f'Bot "{bot.user.name}" has connected to Discord. Starting log')
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     if message.guild is None: # don't use bot commands in DMs
-#         await message.reply("I can't respond to messages in DMs. Please use my slash commands in the server.")
-#         return
-    
-#     await bot.process_commands(message)
-
 
 @bot.tree.error
-async def on_app_command_error(interaction, error):
+async def on_app_command_error(interaction: Interaction, error: errors.DiscordException):
+    """[error] Handles slash command errors
+
+    Parameters
+    ----------
+    interaction : discord.Interaction
+        The interaction object that initiated the command
+    error : discord.errors.DiscordException
+        The error that occurred
+
+    Raises
+    ------
+    error
+        If the interaction is expired and cannot be responded to, simply raise the error
+    """
     global_utils.debug_log(str(error))
 
     if interaction.is_expired():
@@ -43,7 +50,16 @@ async def on_app_command_error(interaction, error):
 
 
 @bot.event
-async def on_command_error(ctx, error):
+async def on_command_error(ctx: Context, error: errors.DiscordException):
+    """[error] Handles text command errors
+
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        The context object that initiated the command
+    error : discord.errors.CommandError
+        The error that occurred
+    """
     global_utils.debug_log(str(error))
 
     if ctx.author.id == global_utils.my_id:
@@ -56,7 +72,7 @@ async def on_command_error(ctx, error):
 
 async def main():
     await global_utils.load_cogs(bot)
-    # await bot.start(global_utils.bot_token)
+    await bot.start(global_utils.bot_token)
 
 asyncio.run(main())
 bot.run(global_utils.bot_token)
