de604a76a81e470c3335cacbeecb5ba012956722
ibartlett2 <ibartlett2@sycamores.indstate.edu>
2024-05-31 22:53:10 -0400

    see last commit

--- cogs/tasks_cog.py
+++ cogs/tasks_cog.py
@@ -25,7 +25,7 @@ class TasksCog(commands.Cog):
         self.latest_log.start()
 
     # @tasks.loop(hours=1)
-    @tasks.loop(hours=global_utils.premier_reminder_times)
+    @tasks.loop(time=global_utils.premier_reminder_times)
     async def eventreminders(self):
         """[task] Sends reminders for upcoming events near starting times of West Coast premier events"""
 
